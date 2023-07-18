83d6b357aee85efc0f156c6fbc385a1547a929b2
Manu <manuharris0@gmail.com>
2023-07-18 11:12:38 -0300

    last commit

--- client/src/views/Detail/Detail.jsx
+++ client/src/views/Detail/Detail.jsx
@@ -1,11 +1,11 @@
 import { useParams } from "react-router-dom";
 
 const Detail = () => {
-    const id = useParams()
+    const params = useParams()
     return (
         <div>
             Detail page
-            <p>{`Mostrando la receta con id ${id}`}</p>
+            <p>{`Mostrando la receta con id ${params.id}`}</p>
         </div>
     );
 };
